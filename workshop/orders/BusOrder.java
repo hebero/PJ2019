0195cefb20abc1798ffb5940939e98cef857d4d0
Heber <heberdanielorellana@gmail.com>
2019-10-06 10:50:20 -0600

    Primer commit con las ordenes de automoviles

---
+++ workshop/orders/BusOrder.java
@@ -0,0 +1,34 @@
+package workshop.orders;
+
+/**
+ * BusOrder
+ * author:
+ * carnet:
+ */
+public class BusOrder extends PaintOrder {
+
+    protected BusOrder(int number, String plate, int total, double time) {
+        super(number, plate, total, time);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPrice() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void end() {
+        // TODO Auto-generated method stub
+
+    }
+
+    
+}
---
+++ workshop/orders/CouperOrder.java
@@ -0,0 +1,34 @@
+package workshop.orders;
+
+/**
+ * CouperOrder
+ * author: Heber Orellana
+ * carnet: 190033015
+ */
+public class CouperOrder extends PaintOrder{
+
+    protected CouperOrder(int number, String plate, int total, double time) {
+        super(number, plate, total, time);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPrice() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void end() {
+        // TODO Auto-generated method stub
+
+    }
+
+    
+}
